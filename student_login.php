89fddb659eb37bfcecd48607a34c365918cae096
Apoorva Vyas <apoorva.vyas3880@gmail.com>
2020-02-12 14:30:37 +0530

    Student login by Apoorva

---
+++ student_login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <a href="signin.html">Register</a>
+   <!----<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <meta http-equiv = "refresh" content = " time ; url = "/>
+    
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+<link href="css/global.css" type="text/css" rel="stylesheet">
+
+
+<script src="https://ajax.googlepis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container-fluid bg">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-12"></div>
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <form class="form-container">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email </label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      
+                    </div>
+                    <div class="">
+                      <label>
+                        <input type="checkbox"> Remember me
+                      </label>
+                    </div>
+                    <button type="submit" class="btn btn-success btn-block">Login</button>
+                    <button type="submit" class="btn btn-success btn-block">Register</button>
+                </form>
+
+            </div>
+       <div class="col-md-4 col-sm-4 col-xs-12"></div>
+
+       </div>
+       </div>
+   
+      
+      
+</body>
+</html>
