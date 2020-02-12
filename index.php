87aba9a53d1e6c6e82d796e99a12c73b7b2aeb6e
Sandiplaha <45128751+Sandylaha@users.noreply.github.com>
2020-02-12 13:14:12 +0530

    index.php updated

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+Author : Mavoix Solutions Pvt Ltd
+Ramp-up: Virtual Intern Assignment#1
+====================================
+Version : v1.0
+Date : 01-Jan-2020
+====================================
+-->
+<Html>
+<head>
+<meta charset= "utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Index</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link rel="stylesheet" href="default.css"  type="text/css" media="all" />
+<link rel="stylesheet" href="fonts.css" type="text/css" media="all" />
+<<link rel="stylesheet" href="\mavoix\k3project\sandbox\css\style_for_index.css" type="text/css" media="all" />
+</head>
+<body><br/><br/>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1></span><a href="#">College Name</a></h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="#" accesskey="1" title="">Home</a></li>
+				<li><a href="#" accesskey="2" title="">Faculty</a></li>
+				<li><a href="#" accesskey="3" title="">Placement</a></li>
+				<li><a href="#" accesskey="4" title="">Events</a></li>
+				<li><a href="#" accesskey="5" title="">Contact Us</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div id="header-featured">
+	<div id="banner" class="container" > </div>
+</div>
+
+<div id="wrapper">
+	<div id="featured-wrapper">
+		<div id="featured" class="extra2 margin-btm container">
+			<div class="main-title">
+				<h2>Visvesvaraya Technological University</h2>
+				<span class="byline">VTU is a State University in Karnataka State of the Republic of India</span> </div>
+			<div class="ebox1"> <span class="fa fa-pagelines"></span>
+				<div class="title">
+					<h2>For Campus Login</h2>
+					<span class="byline">For all campuses under VTU</span> </div>
+					<p>VTU is one of the largest universities in India with 218 colleges affiliated to it with an intake capacity of over 467,100 undergraduate students and 12,666 postgraduate students. The university encompasses technical and management fields which offer 30 undergraduate and 71 postgraduate courses.It has around 1800 PhD candidates.</p>
+				<a href="\mavoix\k3project\sandbox\login_for_campus.php" class="button">Login Here</a> </div>
+			<div class="ebox2"> <span class="fa fa-anchor"></span>
+				<div class="title">
+					<h2>For Student Login</h2>
+					<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span> </div>
+				<p>Donec pulvinar ullamcorper metus. In eu odio at lectus pulvinar mollis. Vestibulum sem magna, elementum ut, vestibulum eu, facilisis quis, arcu. Mauris a dolor. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed blandit. Phasellus pellentesque, ante nec iaculis dapibus, eros justo auctor lectus.</p>
+				<a href="#" class="button">Etiam posuere</a> </div>
+		</div>
+	</div>
+</div>
+<div id="copyright" class="container">
+<div class="cont" align="center">
+<input type="text" placeholder="Name"/>	
+<input type="text" placeholder="Email"/><br/>
+<input type="text" placeholder="Subject"/>	
+<input type="text" placeholder="Contact"/>	
+<br/>
+	<textarea rows = "5" cols = "60" name = "description" placeholder="Enter your Message here"></textarea><br/>
+    <input type="submit" name="submit" value="Send Message" />
+  </div>
+  <br/>
+	<p>&copy; All rights reserved | Footer Details | Design by Mavoix Solutions</p>
+</div>
+</body>
+</Html>
