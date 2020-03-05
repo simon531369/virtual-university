8974a76316be444041504375cb9b43a38e798bf6
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-05 16:26:40 +0530

    Update login_for_campus.php

--- api/login_for_campus.php
+++ api/login_for_campus.php
@@ -1,5 +1,5 @@
 <?php
-include "api/connection.php";
+include "connection.php";
 ?>
 <html>
 <head>
@@ -377,5 +377,36 @@ body{
 </body>
 </html>
 <?php
-include "api/campus_login.php";
+if(isset($_POST['but_submit'])){
+
+  $username = $_POST['txt_uname'];
+    $password =$_POST['txt_pwd'];
+
+    if ($username != "" && $password != ""){
+
+        $sql_query = "select count(*) as cntUser from system_users where username='".$username."' and password='".$password."'";
+        $result = mysqli_query($conn,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0){
+            $_SESSION['username'] = $username;
+            echo "<script>
+            alert('Login Successful');
+            window.location.href='campusdashboard.php';
+            </script>";
+        }   
+        else{
+          echo "<script>
+            alert('Invalid Login');
+            </script>";
+    }
+  }
+    else{
+      echo "<script>
+            alert('Please fill the Details');
+            </script>";   
+  }
+}
 ?>
