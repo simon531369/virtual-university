ae8645fea1b3b49c60b61700ade36dc18f62ca87
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-05 19:59:11 +0530

    CLI tested command for DB connection

--- api/connection.php
+++ api/connection.php
@@ -1,13 +1,13 @@
 <?php 
-$db = mysqli_init( );
-mysqli_options($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
-mysqli_ssl_set($db, NULL, NULL, '/var/tmp/rds-ca-2019-root.pem', NULL, NULL);
-$conn = mysqli_real_connect($db, 'k3beta.c7lkgbzlct6d.ap-south-1.rds.amazonaws.com', 'root', 'maV01X615', 'k3beta', 3306, NULL, MYSQLI_CLIENT_SSL);
-mysqli_select_db($conn,'k3beta');
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+$db = mysqli_init();
+mysqli_options ($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+$db->ssl_set(NULL, NULL, '/var/tmp/rds-ca-2019-root.pem', NULL, NULL);
+$conn = mysqli_real_connect ($db, 'k3beta.c7lkgbzlct6d.ap-south-1.rds.amazonaws.com', 'root', 'maV01X615', 'k3beta', 3306, NULL, MYSQLI_CLIENT_SSL);
+if (!$conn)
+{
+    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
+} else {
+   
 $db_name='k3beta';
 $app_name="InternBuddy";
 $app_version="1.0";
@@ -19,5 +19,6 @@ $footer="<a href='http://www.mavoix.in' target='_blank' class='text-bold-800 gre
 $project_name="InternBuddy";
 $footer_content="Designed & Developed By:";
 $developed_by="<a href='http://www.mavoix.in' target='_blank' class='text-bold-800 grey darken-2'>Mavoix Solutions Pvt Ltd</a>";
+}
 ?>
 
